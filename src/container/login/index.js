90ef85e0d44a98a7c236a7ef89257d48d02f46ab
gmw-zjw <gaomingwei2017@163.com>
2018-01-22 21:23:05 +0800

    remove

--- src/container/login/index.js
+++ src/container/login/index.js
@@ -7,10 +7,10 @@ class Login extends React.Component {
 
     constructor(props) {
         super(props);
+        
 
-        // 初始化状态
         this.state = {
-
+            form: Form.create()
         }
 
     }
@@ -24,23 +24,42 @@ class Login extends React.Component {
         });
     }
     
-    render() {
-
-        const {getFieldDecorator} = this.props.form;
+    render() {        
+        // const { getFieldDecorator } = this.props.form;
 
         return (
-            <Form onSubmit={this.hanldeSubmit} className='login-form'>
-                <FormItem>
-                    {getFieldDecorator('userName', {
-                        rules: [{}]
-                    })}
-
-                </FormItem>
-                <FormItem>
-
-
-                </FormItem>
-            </Form>
+            // <Form onSubmit={this.hanldeSubmit} className='login-form'>
+            //     <FormItem>
+            //         {getFieldDecorator('userName', {
+            //             rules: [{ required: true, message: 'Please input your username!' }],
+            //         })(
+            //             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            //         )}
+            //     </FormItem>
+            //     <FormItem>
+            //         {getFieldDecorator('password', {
+            //             rules: [{ required: true, message: 'Please input your Password!' }],
+            //         })(
+            //             <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            //         )}
+            //     </FormItem>
+            //     <FormItem>
+            //         {getFieldDecorator('remember', {
+            //             valuePropName: 'checked',
+            //             initialValue: true,
+            //         })(
+            //             <Checkbox>Remember me</Checkbox>
+            //         )}
+            //         <a className="login-form-forgot" href="">Forgot password</a>
+            //         <Button type="primary" htmlType="submit" className="login-form-button">
+            //             Log in
+            //         </Button>
+            //         Or <a href="">register now!</a>
+            //     </FormItem>
+            // </Form>
+        
+            <Button type="primary">注册</Button>
+        
         );
     }
 }
