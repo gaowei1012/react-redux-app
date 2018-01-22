d18f6c59e3e7e6f26584119bbe5b8cf13ec8c725
gmw-zjw <gaomingwei2017@163.com>
2018-01-22 17:11:10 +0800

    login

--- src/container/login/index.js
+++ src/container/login/index.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import {Form, Icon, Input, Button, Checkbox} from 'antd'
+
+const FormItem = Form.Item;
 
 class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        // 初始化状态
+        this.state = {
+
+        }
+
+    }
+
+    hanldeSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('///', values);
+            }
+        });
+    }
+    
     render() {
+
+        const {getFieldDecorator} = this.props.form;
+
         return (
-            <h2>登录页</h2>
+            <Form onSubmit={this.hanldeSubmit} className='login-form'>
+                <FormItem>
+                    {getFieldDecorator('userName', {
+                        rules: [{}]
+                    })}
+
+                </FormItem>
+                <FormItem>
+
+
+                </FormItem>
+            </Form>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 
-import 'antd/dist/antd.css'
-
 import registerServiceWorker from './registerServiceWorker'
 import reducers from './reducer'
 // import config from './config'
+
+import 'antd/dist/antd.css'
 
 import Login from './container/login'
 import Register from './container/register'
